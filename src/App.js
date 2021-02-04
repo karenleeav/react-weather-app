25961489cc1da381d83468ac028a267de4fddd99
karenleeav <74217035+karenleeav@users.noreply.github.com>
2021-02-04 15:11:21 -0700

    imported bootstrap from terminal

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import Weather from "./Weather";
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
         <Weather />
         <footer>
         <a href="https://github.com/karenleeav/react-weather-app">Open-source code</a> by Karen Avidar
         </footer>
+        </div>
     </div>
   );
 }
