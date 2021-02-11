322182d47e89ddca54a5fc60e95ea347f69c7d71
karenleeav <74217035+karenleeav@users.noreply.github.com>
2021-02-11 12:18:08 -0700

    Added external icons with new WeatherIcon component and changed CSS

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
+import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherInfo(props){
     return(<div className = "WeatherInfo">
@@ -15,7 +16,10 @@ export default function WeatherInfo(props){
       <div className="row">
         <div className="col-6">
           <div className="clearfix weather-temperature d-flex">
-            <img src={props.data.imgUrl} alt={props.data.description} className="float-left" />
+              <div className="float-left">
+              <WeatherIcon code={props.data.icon} alt={props.data.description} />
+              </div>
+          
             <div className="float-left">
               <strong>{Math.round(props.data.temperature)}</strong>
               <span className="units">
